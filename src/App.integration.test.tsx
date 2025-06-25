f9cc941cdec7e189e5b6c43eb37aabd392240b97
nym-tmn <nymtmn8690@gmail.com>
2025-06-25 22:50:35 +0500

    Added additional checks to the integration test

--- src/App.integration.test.tsx
+++ src/App.integration.test.tsx
@@ -1,13 +1,63 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
-import { authorization } from "./api/api";
+import { authorization, getUsers } from "./api/api";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 import { AuthProvider } from "./providers/AuthProvider";
 
 vi.mock('./api/api', () => ({
 	authorization: vi.fn(),
+	getUsers: vi.fn(),
 }));
+
+const mockUsers = [
+	{
+		"id": 1,
+		"name": "Leanne Graham",
+		"username": "Bret",
+		"email": "Sincere@april.biz",
+		"address": {
+			"street": "Kulas Light",
+			"suite": "Apt. 556",
+			"city": "Gwenborough",
+			"zipcode": "92998-3874",
+			"geo": {
+				"lat": "-37.3159",
+				"lng": "81.1496"
+			}
+		},
+		"phone": "1-770-736-8031 x56442",
+		"website": "hildegard.org",
+		"company": {
+			"name": "Romaguera-Crona",
+			"catchPhrase": "Multi-layered client-server neural-net",
+			"bs": "harness real-time e-markets"
+		}
+	},
+	{
+		"id": 2,
+		"name": "Ervin Howell",
+		"username": "Antonette",
+		"email": "Shanna@melissa.tv",
+		"address": {
+			"street": "Victor Plains",
+			"suite": "Suite 879",
+			"city": "Wisokyburgh",
+			"zipcode": "90566-7771",
+			"geo": {
+				"lat": "-43.9509",
+				"lng": "-34.4618"
+			}
+		},
+		"phone": "010-692-6593 x09125",
+		"website": "anastasia.net",
+		"company": {
+			"name": "Deckow-Crist",
+			"catchPhrase": "Proactive didactic contingency",
+			"bs": "synergize scalable supply-chains"
+		}
+	},
+]
 
 describe('Intergration test: flow authorization', () => {
 
@@ -20,8 +70,11 @@ describe('Intergration test: flow authorization', () => {
 		)
 	});
 
-	it('user successfully logs in through the form', async () => {
-		vi.mocked(authorization).mockResolvedValueOnce({});
+	it('The user successfully logs in through the form and then logs out of the account', async () => {
+		vi.mocked(authorization).mockImplementationOnce(() =>
+			new Promise((resolve) => setTimeout(resolve, 100))
+		);
+		vi.mocked(getUsers).mockResolvedValue(mockUsers);
 
 		await userEvent.click(screen.getByRole('button', { name: /^Авторизоваться$/ }));
 
@@ -37,34 +90,36 @@ describe('Intergration test: flow authorization', () => {
 
 		await userEvent.click(screen.getByRole('button', { name: /^Войти$/ }));
 
+
+		expect(screen.queryByRole('button', { name: /^Войти$/ })).not.toBeInTheDocument();
+		expect(screen.getByRole('button', { name: /^Processing...$/ })).toBeDisabled();
+		expect(screen.getByRole('button', { name: /^Processing...$/ })).toHaveTextContent('Processing...');
 		expect(authorization).toHaveBeenCalledWith({
 			email: 'test@mail.ru',
 			password: '12345678'
 		});
 
+		await waitFor(() => {
+			expect(getUsers).toHaveBeenCalledOnce();
+		})
+
 		expect(screen.queryByTestId('login-modal')).toBeNull();
 		expect(screen.getByText(/^Вы авторизованы!!!$/)).toBeInTheDocument();
 		expect(screen.getByRole('button', { name: /^Выйти$/ })).toBeInTheDocument();
-	});
+		expect(screen.getByText(/^Список пользователей:$/)).toBeInTheDocument();
+		expect(screen.getByTestId('users-list')).toBeInTheDocument();
 
-	it('disables login button during submission', async () => {
-		vi.mocked(authorization).mockImplementationOnce(() =>
-			new Promise((resolve) => setTimeout(resolve, 100))
-		);
+		mockUsers.forEach(user => {
+			expect(screen.getByText(user.name)).toBeInTheDocument();
+		})
 
-		await userEvent.click(screen.getByRole('button', { name: /^Авторизоваться$/ }));
-		await userEvent.type(screen.getByTestId('email-input'), 'test@mail.ru');
-		await userEvent.type(screen.getByTestId('password-input'), '12345678');
-		await userEvent.click(screen.getByRole('button', { name: /^Войти$/ }));
+		await userEvent.click(screen.getByRole('button', { name: /^Выйти$/ }));
 
-		expect(screen.queryByRole('button', { name: /^Войти$/ })).not.toBeInTheDocument();
-		expect(screen.getByRole('button', { name: /^Processing...$/ })).toBeDisabled();
-		expect(screen.getByRole('button', { name: /^Processing...$/ })).toHaveTextContent('Processing...');
-
-		await waitFor(() => {
-			expect(screen.getByText(/^Вы авторизованы!!!$/)).toBeInTheDocument();
-			expect(screen.getByRole('button', { name: /^Выйти$/ })).toBeInTheDocument();
-		});
+		expect(screen.getByRole('button', { name: /^Авторизоваться$/ })).toBeInTheDocument();
+		expect(screen.queryByText(/^Вы авторизованы!!!$/)).not.toBeInTheDocument();
+		expect(screen.queryByRole('button', { name: /^Выйти$/ })).not.toBeInTheDocument();
+		expect(screen.queryByText(/^Список пользователей:$/)).not.toBeInTheDocument();
+		expect(screen.queryByTestId('users-list')).not.toBeInTheDocument();
 	});
 
 	it('should clear form after successful submission', async () => {
@@ -88,7 +143,7 @@ describe('Intergration test: flow authorization', () => {
 		expect(screen.queryByText('general-error')).toBeNull();
 	});
 
-	it('should persist form data when submission fails', async () => {
+	it('should save form data when submission fails', async () => {
 		vi.mocked(authorization).mockRejectedValueOnce(new Error('Неверный логин или пароль'));
 
 		await userEvent.click(screen.getByRole('button', { name: /^Авторизоваться$/ }));
@@ -120,6 +175,8 @@ describe('Intergration test: flow authorization', () => {
 			email: 'wrong@gmail.com',
 			password: '87654321'
 		});
+
+		expect(getUsers).not.toHaveBeenCalledOnce();
 
 		expect(await screen.findByTestId('general-error')).toBeInTheDocument();
 		expect(await screen.findByTestId('general-error')).toHaveTextContent('Неверный логин или пароль');
@@ -168,6 +225,7 @@ describe('Intergration test: flow authorization', () => {
 
 		expect(screen.getByTestId('email-input')).toHaveValue('test@');
 		expect(screen.getByTestId('password-input')).toHaveValue('@#$#wbewbh');
+		expect(authorization).not.toHaveBeenCalledOnce();
 
 		await userEvent.clear(screen.getByTestId('email-input'));
 		await userEvent.clear(screen.getByTestId('password-input'));
@@ -176,6 +234,7 @@ describe('Intergration test: flow authorization', () => {
 
 		expect(screen.getByTestId('email-input')).toHaveValue('test@');
 		expect(screen.getByTestId('password-input')).toHaveValue('12345678');
+		expect(authorization).not.toHaveBeenCalledOnce();
 
 		await userEvent.clear(screen.getByTestId('email-input'));
 		await userEvent.clear(screen.getByTestId('password-input'));
@@ -184,6 +243,7 @@ describe('Intergration test: flow authorization', () => {
 
 		expect(screen.getByTestId('email-input')).toHaveValue('test@mail.ru');
 		expect(screen.getByTestId('password-input')).toHaveValue('@#$#wbewbh');
+		expect(authorization).not.toHaveBeenCalledOnce();
 	});
 
 	it('clears form data and errors when modal is closed', async () => {
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import userEvent from "@testing-library/user-event";
 import { useAuth } from "./hooks/isAuth/useAuth";
@@ -28,6 +28,55 @@ vi.mock('./components/Button/Button', () => ({
 	))
 }))
 
+const mockUsers = [
+	{
+		"id": 1,
+		"name": "Leanne Graham",
+		"username": "Bret",
+		"email": "Sincere@april.biz",
+		"address": {
+			"street": "Kulas Light",
+			"suite": "Apt. 556",
+			"city": "Gwenborough",
+			"zipcode": "92998-3874",
+			"geo": {
+				"lat": "-37.3159",
+				"lng": "81.1496"
+			}
+		},
+		"phone": "1-770-736-8031 x56442",
+		"website": "hildegard.org",
+		"company": {
+			"name": "Romaguera-Crona",
+			"catchPhrase": "Multi-layered client-server neural-net",
+			"bs": "harness real-time e-markets"
+		}
+	},
+	{
+		"id": 2,
+		"name": "Ervin Howell",
+		"username": "Antonette",
+		"email": "Shanna@melissa.tv",
+		"address": {
+			"street": "Victor Plains",
+			"suite": "Suite 879",
+			"city": "Wisokyburgh",
+			"zipcode": "90566-7771",
+			"geo": {
+				"lat": "-43.9509",
+				"lng": "-34.4618"
+			}
+		},
+		"phone": "010-692-6593 x09125",
+		"website": "anastasia.net",
+		"company": {
+			"name": "Deckow-Crist",
+			"catchPhrase": "Proactive didactic contingency",
+			"bs": "synergize scalable supply-chains"
+		}
+	},
+]
+
 describe('App component', () => {
 
 	beforeEach(() => {
@@ -44,7 +93,7 @@ describe('App component', () => {
 		expect(screen.getByTestId('app')).toMatchSnapshot();
 	});
 
-	it('should render greeting and button with text "Exit" if authorized', () => {
+	it('should render greeting and button with text "Exit" if authorized', async () => {
 
 		vi.mocked(useAuth).mockReturnValue({
 			isAuth: true,
@@ -56,6 +105,17 @@ describe('App component', () => {
 
 		expect(screen.getByText(/^Вы авторизованы!!!$/)).toBeInTheDocument()
 		expect(screen.getByRole('button', { name: /^Выйти$/ })).toBeInTheDocument();
+		expect(screen.getByText(/^Список пользователей:$/)).toBeInTheDocument();
+
+		await waitFor(() => {
+			expect(screen.getByTestId('users-list')).toBeInTheDocument();
+			mockUsers.forEach(user => {
+				expect(screen.getByText(user.name)).toBeInTheDocument();
+			})
+
+		})
+
+
 		expect(screen.queryByText('Авторизоваться')).toBeNull();
 		expect(screen.getByTestId('app')).toMatchSnapshot();
 	});
